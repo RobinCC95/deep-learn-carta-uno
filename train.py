7a819976125e34a59b76eca81878cf097f4eb8b1
Robin <robinson.1701625954@ucaldas.edu.co>
2023-05-24 09:36:20 -0500

    test image recorte

---
+++ train.py
@@ -0,0 +1,7 @@
+import tensorflow as tf
+import keras
+import numpy as np
+import cv2
+###Importar componentes de la red neuronal
+from keras.models import Sequential
+from keras.layers import InputLayer,Input,Conv2D, MaxPool2D,Reshape,Dense,Flatten
