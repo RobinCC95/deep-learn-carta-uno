e288390521b0349c4b85d66d2a5815d92ec0bb3b
Robin <robinson.1701625954@ucaldas.edu.co>
2023-05-25 14:42:17 -0500

    add dataset train

--- rotate.py
+++ rotate.py
@@ -1,14 +1,44 @@
 import cv2
 import numpy as np
 
+ruta = "dataset/train"
+ruta_origin = "assets/image"
+def rotarImagen(img, grados, tamanio=(128, 128)):
+    # img = cv2.resize(img, (128, 128))
+    rows, cols = img.shape[:2]
+    # Calcula el tamaño del cuadro de límite después de la rotación
+    M = cv2.getRotationMatrix2D((cols / 2, rows / 2), grados, 1)
+    cos = np.abs(M[0, 0])
+    sin = np.abs(M[0, 1])
+    new_cols = int((rows * sin) + (cols * cos))
+    new_rows = int((rows * cos) + (cols * sin))
 
-img = cv2.imread("assets/c_2.png")
-def rotarImagen(img,grados):
-    rows,cols = img.shape[:2]
-    M = cv2.getRotationMatrix2D((cols/2,rows/2),grados,1)
-    dst = cv2.warpAffine(img,M,(cols,rows))
-    return dst
-# realiza una rotacion
-rotacion=rotarImagen(img,45)
-# guarda la imagen en la carpeta assets/rotacion_{grados}.jpg
-cv2.imwrite("assets/rotacion_45.jpg",rotacion)
+    # Ajusta la matriz de rotación para evitar recorte
+    M[0, 2] += (new_cols / 2) - (cols / 2)
+    M[1, 2] += (new_rows / 2) - (rows / 2)
+
+    # Aplica la rotación a la imagen sin recortar
+    dst = cv2.warpAffine(img, M, (new_cols, new_rows))
+    gray = cv2.cvtColor(dst, cv2.COLOR_BGR2GRAY)
+    #redimentciona la imagen
+    img_redim = cv2.resize(gray, tamanio)
+    return img_redim
+
+def save_dataset(ruta, rut_img_ini, limite= 10, pasos_grados= 5):
+    for categoria in range (limite):
+        ruta_origin_img = rut_img_ini+"/"+ str(categoria)+ ".png"
+        print(ruta_origin_img)
+        for grados in range (0,360,pasos_grados):
+            ruta_save = ruta+"/"+str(categoria)+"/"+str(categoria)+"_"+str(grados)+".jpg"
+            print(ruta_save)
+            img = cv2.imread(ruta_origin_img)
+            cv2.imwrite(ruta_save, rotarImagen(img, grados))
+
+
+save_dataset(ruta, ruta_origin)
+
+
+# Ejemplo de uso
+# img = cv2.imread("assets/c_1.png")
+# rotacion = rotarImagen(img, 35)
+# cv2.imwrite("assets/rotacion_70.jpg", rotacion)
