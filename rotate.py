4452c79a54db14b102077e0ae01ef3299c515de5
Robin <robinson.1701625954@ucaldas.edu.co>
2023-05-27 11:50:47 -0500

    documentation function

--- rotate.py
+++ rotate.py
@@ -4,7 +4,13 @@ import numpy as np
 ruta = "dataset/train"
 ruta_origin = "assets/image"
 def rotarImagen(img, grados, tamanio=(128, 128)):
-    # img = cv2.resize(img, (128, 128))
+    """
+    funcion para rotar una imagen
+    :param img: imagen a rotar
+    :param grados: grados a rotar la imagen
+    :param tamanio: tamaño de la imagen
+    :return:  imagen rotada
+    """
     rows, cols = img.shape[:2]
     # Calcula el tamaño del cuadro de límite después de la rotación
     M = cv2.getRotationMatrix2D((cols / 2, rows / 2), grados, 1)
@@ -25,6 +31,15 @@ def rotarImagen(img, grados, tamanio=(128, 128)):
     return img_redim
 
 def save_dataset(ruta, rut_img_ini, limite= 10, pasos_grados= 5):
+    """
+    funcion para guardar el dataset de imagenes rotadas en una carpeta
+
+    :param ruta: ruta donde se guardara el dataset
+    :param rut_img_ini:  ruta de las imagenes originales
+    :param limite:  limite de imagenes a rotar
+    :param pasos_grados:  pasos de grados a rotar
+    :return:  None
+    """
     for categoria in range (limite):
         ruta_origin_img = rut_img_ini+"/"+ str(categoria)+ ".png"
         print(ruta_origin_img)
